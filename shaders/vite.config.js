d45fb8dca210974ddd57b10d09b26d2b27037c46
Steven Maribojoc <steven.maribojoc@gmail.com>
2026-07-28 19:32:53 -0700

    fix base paths and rebuild all projects

--- shaders/vite.config.js
+++ shaders/vite.config.js
@@ -3,7 +3,7 @@ import glsl from 'vite-plugin-glsl'
 export default {
     root: 'src/',
     publicDir: '../static/',
-    base: './',
+    base: '/shaders/dist/',
     server:
     {
         host: true, // Open to local network and display URL
