0d9a586fa7ba9e64eeef18808a53f95fb1f1a553
Steven Maribojoc <steven.maribojoc@gmail.com>
2026-07-29 00:39:49 -0700

    Fixing bug with reset button in debugger

--- physics-simulation/src/script.js
+++ physics-simulation/src/script.js
@@ -283,15 +283,11 @@ window.addEventListener('resize', () =>
 // Reset
 debugObject.reset = () => {
     for (const object of objectsToUpdate){
-        // Remove body
         object.body.removeEventListener('collide', playHitSound)
         world.removeBody(object.body)
-
-        // Remove mesh
         scene.remove(object.mesh)
-
-        objectsToUpdate.splice(0, objectsToUpdate.length)
     }
+    objectsToUpdate.splice(0, objectsToUpdate.length)
 }
 gui.add(debugObject, 'reset')
 
